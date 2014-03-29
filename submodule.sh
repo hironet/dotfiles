c83772ee6187e255fbd4d32a304bdaea7369032d
Hiro <hiroyuki1126@gmail.com>
2014-03-29 22:27:05 +0900

    軽微な修正。

--- submodule.sh
+++ submodule.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-git submodule init
-git submodule update
+git clone https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
+git clone https://github.com/sstephenson/rbenv.git .rbenv
+mkdir .rbenv/plugins
+git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins
 
 exit 0
