663f4b04323e5a0a2b121e718e0e1aa23597e3be
Hiro <hiroyuki1126@gmail.com>
2014-03-29 23:54:01 +0900

    rbenvを削除。

--- submodule.sh
+++ submodule.sh
@@ -3,8 +3,4 @@
 mkdir -p .vim/bundle
 git clone https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
 
-git clone https://github.com/sstephenson/rbenv.git .rbenv
-mkdir -p .rbenv/plugins
-git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins
-
 exit 0
