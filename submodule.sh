4034042fa280ca145c0b4bd235cf8b111b5d73cc
Hiro <hiroyuki1126@gmail.com>
2014-06-21 22:18:33 +0900

    submodule.shを削除。

--- submodule.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p .vim/bundle
-git clone https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
-
-git clone https://github.com/sstephenson/rbenv.git .rbenv
-git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins/ruby-build
-
-exit 0
