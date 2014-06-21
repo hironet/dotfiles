a192d36c6762232b4678404f31b71312bf54788a
Hiro <hiroyuki1126@gmail.com>
2014-06-21 19:12:30 +0900

    rbenvを追加。

--- submodule.sh
+++ submodule.sh
@@ -3,4 +3,7 @@
 mkdir -p .vim/bundle
 git clone https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
 
+git clone https://github.com/sstephenson/rbenv.git .rbenv
+git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins/ruby-build
+
 exit 0
