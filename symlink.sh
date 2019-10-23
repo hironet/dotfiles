60a42b99a09817499f2e84a7fd05a361e79fce14
Hiro <hiroyuki1126@gmail.com>
2019-10-23 15:08:44 +0900

    lnコマンドのオプション"-i"を削除し、"-b"を追加。

--- symlink.sh
+++ symlink.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 for dotfile in .?*; do
     if [ ${dotfile} != '..' ] && [ ${dotfile} != '.git' ]; then
-        ln -Fis "${PWD}/${dotfile}" ${HOME}
+        ln -Fsb "${PWD}/${dotfile}" ${HOME}
     fi
 done
 
