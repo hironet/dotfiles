5e3cf80da1d4022b07577064d8941b574376a6a5
Hiro <hiroyuki1126@gmail.com>
2020-12-31 00:04:55 +0900

    lnに-iオプションを追加。

--- symlink.sh
+++ symlink.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 for dotfile in .?*; do
     if [ ${dotfile} != '..' ] && [ ${dotfile} != '.git' ]; then
-        ln -Fs "${PWD}/${dotfile}" ${HOME}
+        ln -Fis "${PWD}/${dotfile}" ${HOME}
     fi
 done
 
